98ef2815feb7968f5f4263d560c268c4107e94ed
Selim <96252832+Selim-Gunal@users.noreply.github.com>
2023-01-22 21:59:49 +0300

    atm 2.0

---
+++ src/Account.java
@@ -0,0 +1,8 @@
+public class Account {
+    public Account(){
+        int accountNumber = 0;
+        int pin = 0000;
+        double avalibleBalance = 0.0;
+        double totalBalance = 0.0;
+    }
+}
---
+++ src/balanceInquire.java
@@ -0,0 +1,2 @@
+public class balanceInquire {
+}
